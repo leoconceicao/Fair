9c6fe1d43f76fca30c5da0f6e5e499e1c1a79c0e
Leonardo Conceição <leonardo.cognacco2@gmail.com>
2022-04-05 21:30:32 -0300

    Initial commit

---
+++ android/app/src/main/java/br/com/fair/fair/MainActivity.java
@@ -0,0 +1,11 @@
+package br.com.fair.fair;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+
+    public static void main(String[] args) {
+
+    }
+
+}
