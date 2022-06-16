2866923526a8f13cc4d9637b915757d9f0ecb2b4
Henrique Gollnick <henrique.gollnick@gmail.com>
2022-06-16 16:05:43 -0300

    Login Screen

---
+++ fair_app/lib/pages/login.dart
@@ -0,0 +1,107 @@
+import 'package:fair_app/pages/ResetPassword.dart';
+import 'package:fair_app/pages/Signup.dart';
+import 'package:flutter/material.dart';
+
+import '../commons/theme.dart';
+import '../commons/widgets/loginForm.dart';
+import '../commons/widgets/loginOption.dart';
+import '../commons/widgets/primaryButton.dart';
+
+class LogInScreen extends StatelessWidget {
+  const LogInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: kDefaultPadding,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Welcome Back',
+                style: titleText,
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Row(
+                children: [
+                  Text(
+                    'New to this app?',
+                    style: subTitle,
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignUpScreen(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Sign Up',
+                      style: textButton.copyWith(
+                        decoration: TextDecoration.underline,
+                        decorationThickness: 1,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const LogInForm(),
+              const SizedBox(
+                height: 20,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ResetPasswordScreen()));
+                },
+                child: const Text(
+                  'Forgot password?',
+                  style: TextStyle(
+                    color: kZambeziColor,
+                    fontSize: 14,
+                    decoration: TextDecoration.underline,
+                    decorationThickness: 1,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const PrimaryButton(
+                buttonText: 'Log In',
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Or log in with:',
+                style: subTitle.copyWith(color: kBlackColor),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const LoginOption(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
