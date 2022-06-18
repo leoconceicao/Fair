858e1039007628d668aed0209382cd829f127b37
Leonardo Conceição <leonardo.cognacco2@gmail.com>
2022-06-18 17:40:20 -0300

    Commit Validador e Telas

--- fair_app/lib/commons/widgets/SignupForm.dart
+++ fair_app/lib/commons/widgets/SignupForm.dart
@@ -1,15 +1,18 @@
 import 'dart:collection';
 import 'dart:convert';
 
-import 'package:fair_app/commons/ScreenArguments.dart';
-import 'package:fair_app/commons/Theme.dart';
 import 'package:fair_app/commons/widgets/Checkbox.dart';
 import 'package:fair_app/models/FuncionariosModel.dart';
 import 'package:fair_app/models/LojaModel.dart';
 import 'package:fair_app/models/PessoaModel.dart';
 import 'package:flutter/material.dart';
 
-import 'LoginOption.dart';
+import 'package:fair_app/commons/ScreenArguments.dart';
+import 'package:fair_app/commons/Theme.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:validadores/Validador.dart';
+
+import 'package:fair_app/commons/widgets/LoginOption.dart';
 
 class SignUpForm extends StatefulWidget {
   const SignUpForm({Key? key}) : super(key: key);
@@ -19,6 +22,22 @@ class SignUpForm extends StatefulWidget {
 }
 
 class _SignUpFormState extends State<SignUpForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final mascaraTelefone = MaskTextInputFormatter(
+      mask: '(##) #####-####',
+      filter: {"#": RegExp(r'[0-9]')},
+      type: MaskAutoCompletionType.lazy);
+
+  final mascaraCPF = MaskTextInputFormatter(
+      mask: '###.###.###-##',
+      filter: {"#": RegExp(r'[0-9]')},
+      type: MaskAutoCompletionType.lazy);
+
+  final mascaraCNPJ = MaskTextInputFormatter(
+      mask: '##.###.###/####-##',
+      filter: {"#": RegExp(r'[0-9]')},
+      type: MaskAutoCompletionType.lazy);
   bool _isObscure = true;
   bool _isCNPJ = false;
   final TextEditingController _nomeController = TextEditingController();
@@ -35,14 +54,16 @@ class _SignUpFormState extends State<SignUpForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Form(
+      key: _formKey,
+        child:Column(
       children: [
-        buildInputForm('Nome', _nomeController, false),
-        buildInputForm('Email', _emailController, false),
-        buildInputForm('CPF', _cpfController, false),
-        buildInputForm('Telefone', _telefoneController, false),
-        buildInputForm('Senha', _senhaController, true),
-        buildInputForm('Confirmar Senha', _confirmaSenhaController, true),
+        buildInputForm('Nome', _nomeController, false, TextInputType.text),
+        buildInputForm('Email', _emailController, false, TextInputType.emailAddress),
+        buildInputFormValidadorComMascara('CPF', _cpfController, false, mascaraCPF, TextInputType.number, Validar.CPF, 'CPF inválido'),
+        buildInputFormComMascara('Telefone', _telefoneController, false, mascaraTelefone),
+        buildInputForm('Senha', _senhaController, true, TextInputType.text),
+        buildInputForm('Confirmar Senha', _confirmaSenhaController, true, TextInputType.text),
         CheckboxListTile(
           title: const Text(
               'É um fornecedor? Cadastre as informações da sua empresa!'),
@@ -56,17 +77,13 @@ class _SignUpFormState extends State<SignUpForm> {
         Visibility(
           visible: _isCNPJ, // bool
           child: Column(children: [
-            buildInputForm('Nome da Loja', _nomeLojaController, false),
-            buildInputForm('CNPJ', _cnpjController, false),
-            buildInputForm('Telefone da Loja', _telefoneLojaController, false),
+            buildInputForm('Nome da Loja', _nomeLojaController, false, TextInputType.text),
+            buildInputFormValidadorComMascara('CNPJ', _cnpjController, false, mascaraCNPJ, TextInputType.number, Validar.CNPJ, 'CNPJ inválido'),
+            buildInputFormComMascara('Telefone da Loja', _telefoneLojaController, false, mascaraTelefone),
           ]),
         ),
         const SizedBox(
           height: 20,
-        ),
-        const Padding(
-          padding: kDefaultPadding,
-          child: CheckBox('Eu concordo com os Termos e Serviços'),
         ),
         const SizedBox(
           height: 20,
@@ -106,14 +123,15 @@ class _SignUpFormState extends State<SignUpForm> {
           height: 20,
         ),
       ],
-    );
+    ));
   }
 
   Padding buildInputForm(
-      String hint, TextEditingController textEditingController, bool pass) {
+      String hint, TextEditingController textEditingController, bool pass, TextInputType tipoTeclado) {
     return Padding(
         padding: const EdgeInsets.symmetric(vertical: 5),
         child: TextFormField(
+          keyboardType: tipoTeclado != null ? tipoTeclado : TextInputType.text,
           controller: textEditingController,
           obscureText: pass ? _isObscure : false,
           decoration: InputDecoration(
@@ -142,6 +160,134 @@ class _SignUpFormState extends State<SignUpForm> {
         ));
   }
 
+  Padding buildInputFormComMascara(
+      String hint, TextEditingController textEditingController, bool pass, MaskTextInputFormatter tipoMascara) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5),
+        child: TextFormField(
+          inputFormatters: [tipoMascara],
+          controller: textEditingController,
+          obscureText: pass ? _isObscure : false,
+          decoration: InputDecoration(
+            hintText: hint,
+            hintStyle: const TextStyle(color: Colors.black),
+            focusedBorder: const UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.black)),
+            suffixIcon: pass
+                ? IconButton(
+                onPressed: () {
+                  setState(() {
+                    _isObscure = !_isObscure;
+                  });
+                },
+                icon: _isObscure
+                    ? const Icon(
+                  Icons.visibility_off,
+                  color: Colors.green,
+                )
+                    : const Icon(
+                  Icons.visibility,
+                  color: Colors.green,
+                ))
+                : null,
+          ),
+        ));
+  }
+
+  Padding buildInputFormValidadorComMascara(
+      String hint,
+      TextEditingController textEditingController,
+      bool pass,
+      MaskTextInputFormatter mascara,
+      TextInputType tipoTeclado,
+      Validar tipoValidacao,
+      String mensagem) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5),
+        child: TextFormField(
+          inputFormatters: [mascara],
+          keyboardType: tipoTeclado,
+          validator: (value) {
+            // Aqui entram as validações
+            return Validador()
+                .add(tipoValidacao, msg: mensagem)
+                .add(Validar.OBRIGATORIO, msg: 'Campo obrigatório')
+                .valido(value, clearNoNumber: true);
+          },
+          controller: textEditingController,
+          obscureText: pass ? _isObscure : false,
+          decoration: InputDecoration(
+            hintText: hint,
+            hintStyle: const TextStyle(color: Colors.black),
+            focusedBorder: const UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.black)),
+            suffixIcon: pass
+                ? IconButton(
+                onPressed: () {
+                  setState(() {
+                    _isObscure = !_isObscure;
+                  });
+                },
+                icon: _isObscure
+                    ? const Icon(
+                  Icons.visibility_off,
+                  color: Colors.green,
+                )
+                    : const Icon(
+                  Icons.visibility,
+                  color: Colors.green,
+                ))
+                : null,
+          ),
+        ));
+  }
+
+  Padding buildInputFormValidador(
+      String hint,
+      TextEditingController textEditingController,
+      bool pass,
+      TextInputType tipoTeclado,
+      Validar tipoValidacao,
+      String mensagem) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5),
+        child: TextFormField(
+          keyboardType: tipoTeclado,
+          validator: (value) {
+            // Aqui entram as validações
+            return Validador()
+                .add(tipoValidacao, msg: mensagem)
+                .add(Validar.OBRIGATORIO, msg: 'Campo obrigatório')
+                .valido(value, clearNoNumber: true);
+          },
+          controller: textEditingController,
+          obscureText: pass ? _isObscure : false,
+          decoration: InputDecoration(
+            hintText: hint,
+            hintStyle: const TextStyle(color: Colors.black),
+            focusedBorder: const UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.black)),
+            suffixIcon: pass
+                ? IconButton(
+                onPressed: () {
+                  setState(() {
+                    _isObscure = !_isObscure;
+                  });
+                },
+                icon: _isObscure
+                    ? const Icon(
+                  Icons.visibility_off,
+                  color: Colors.green,
+                )
+                    : const Icon(
+                  Icons.visibility,
+                  color: Colors.green,
+                ))
+                : null,
+          ),
+        ));
+  }
+
   void alert(String text) {
     showDialog(
         context: context,
@@ -153,35 +299,39 @@ class _SignUpFormState extends State<SignUpForm> {
   }
 
   void _validarCadastro(context) {
-    PessoaModel pessoaModel = PessoaModel(
-        idPessoa: 0,
-        nome: _nomeController.text,
-        telefone: _telefoneController.text,
-        cpf: _cpfController.text,
-        email: _emailController.text,
-        password: _senhaController.text);
+    final FormState form = _formKey.currentState!;
+    if (form.validate()) {
+      PessoaModel pessoaModel = PessoaModel(
+          idPessoa: 0,
+          nome: _nomeController.text,
+          telefone: _telefoneController.text,
+          cpf: _cpfController.text,
+          email: _emailController.text,
+          password: _senhaController.text);
 
-    late FuncionarioModel funcionarioModel = FuncionarioModel(0, "F", 0, 0);
+      late FuncionarioModel funcionarioModel = FuncionarioModel(0, "F", 0, 0);
 
-    bool success = true;
-    if (_isCNPJ) {
-      LojaModel lojaModel = LojaModel(
-          idLoja: 0,
-          nome: _nomeLojaController.text,
-          cnpj: _cnpjController.text,
-          telefone: _telefoneLojaController.text);
-      LojaModel.addLoja(lojaModel).then((value) => {
-            if (value == "Error")
-              {alert("Erro ao cadastrar loja"), success = false}
-            else
-              {
-                addPessoa(success, pessoaModel, funcionarioModel,
-                    jsonDecode(value).cast<String, dynamic>()["idLoja"])
-              }
-          });
-      funcionarioModel.cargo = "D";
-    } else {
-      addPessoa(success, pessoaModel, funcionarioModel, 0);
+      bool success = true;
+      if (_isCNPJ) {
+        LojaModel lojaModel = LojaModel(
+            idLoja: 0,
+            nome: _nomeLojaController.text,
+            cnpj: _cnpjController.text,
+            telefone: _telefoneLojaController.text);
+        LojaModel.addLoja(lojaModel).then((value) =>
+        {
+          if (value == "Error")
+            {alert("Erro ao cadastrar loja"), success = false}
+          else
+            {
+              addPessoa(success, pessoaModel, funcionarioModel,
+                  jsonDecode(value).cast<String, dynamic>()["idLoja"])
+            }
+        });
+        funcionarioModel.cargo = "D";
+      } else {
+        addPessoa(success, pessoaModel, funcionarioModel, 0);
+      }
     }
   }
 
